c9bad22478e9be133b213af1a26f698a41070a80
Erez Freiberger <erez@epsagon.com>
2018-12-18 11:46:54 +0200

    Fixed First Step - Create a stand alone lambda

--- twitter_service.py
+++ twitter_service.py
@@ -35,10 +35,13 @@ def get_top_tweet(event, _context):
         result_type='mixed',
         since=since_time,
     )
-    body = json.dumps({
-        'screen_name': top_tweet[0].user.screen_name,
-        'tweet': top_tweet[0].text,
-        })
+    if not top_tweet:
+        body = None
+    else:
+        body = json.dumps({
+            'screen_name': top_tweet[0].user.screen_name,
+            'tweet': top_tweet[0].text,
+            })
     return {
         'statusCode': 200,
         'body': body,
